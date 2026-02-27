1254cdb4d12b321862d03716e84f2f93af86339a
kienha2004 <tomkien2022@gmail.com>
2026-02-27 22:32:51 +0700

    new update

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className='header-contents'>
+         <h2>Đặt món ăn yêu thích của bạn tại đây </h2>
+         <p>Chọn món ăn yêu thích và đặt hàng nhanh chóng</p>
+         <button>Xem Menu</button>
+       </div>
+      
+    </div>
+  )
+}
+
+export default Header
