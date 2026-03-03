d3c9ef6f0b9addafe8551c977c676056e97add3b
kienha2004 <tomkien2022@gmail.com>
2026-03-03 16:21:36 +0700

    1

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,9 +1,11 @@
 import { createContext } from 'react';
 import { food_list } from '../assets/assets';
+import { useState } from 'react';
+
 
 export const StoreContext = createContext(null)
 const StoreContextProvider= (props)=>{
-    const [carItems,setItems] = useState({});
+    const [cartItems,setCartItems] = useState({});
     const addToCart = (itemId) =>{
         if (!cartItems[itemId]){
             setCartItems(prev => ({...prev,[itemId]:1}))
