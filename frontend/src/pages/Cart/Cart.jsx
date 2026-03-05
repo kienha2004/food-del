76aaf83c8793c38026a30773ae7878125ddb12cd
kienha2004 <tomkien2022@gmail.com>
2026-03-05 23:01:38 +0700

    new update 3

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "../../context/StoreContext";
 
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
   return (
     <div className="cart">
       <div className="cart-items">
@@ -21,18 +21,54 @@ const Cart = () => {
         {food_list.map((item) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div key={item._id} className="cart-items-item">
+              <div>
+                <div key={item._id} className="cart-items-item">
                 <img src = {item.image} alt=""/>
                 <p>{item.name}</p>
-                <p>{item.price}</p>
+                <p>${item.price}</p>
                 <p>{cartItems[item._id]}</p>
-                <p>{item.price * cartItems[item._id]}</p>
-                <p>x</p>
+                <p>${item.price * cartItems[item._id]}</p>
+                <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
               </div>
+              <hr/>
+                </div>
+              
             );
           }
           return null;
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Tổng số giỏ hàng:</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Tổng phụ</p>
+              <p>{getTotalCartAmount()}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+<p>Phí giao hàng</p>
+<p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+<p>Tổng số</p>
+<p>{getTotalCartAmount()+2}</p>
+            </div>
+            
+          </div>
+          <button>Tiến hành thanh toán</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>Nếu bạn có mã khuyến mãi, hãy nhập mã đó vào đây.</p>
+            <div className='cart-promocode-input'>
+                <input type="text" placeholder='Mã khuyến mãi' />
+                <button>Áp dụng</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
